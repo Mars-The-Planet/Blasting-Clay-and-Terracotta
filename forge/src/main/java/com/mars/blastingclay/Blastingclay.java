3418885c3a84caa26ad30b6fc186b5ec7e29e5e1
Matej <ruzicka.maty@gmail.com>
2025-03-18 16:12:17 +0100

    backported to 1.20.4

---
+++ forge/src/main/java/com/mars/blastingclay/Blastingclay.java
@@ -0,0 +1,10 @@
+package com.mars.blastingclay;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class Blastingclay {
+    public Blastingclay() {
+        CommonClass.init();
+    }
+}
