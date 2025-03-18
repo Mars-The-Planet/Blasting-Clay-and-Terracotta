bd26c1cac155342b5eea991c241b5f825e656ed5
Matej <ruzicka.maty@gmail.com>
2025-03-18 16:13:48 +0100

    backported to 1.20.2

---
+++ fabric/src/main/java/com/mars/blastingclay/Blastingclay.java
@@ -0,0 +1,10 @@
+package com.mars.blastingclay;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Blastingclay implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
